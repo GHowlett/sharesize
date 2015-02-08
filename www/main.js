6d4e798979a1f67e8968b4f43d34667c11c276af
GHowlett <griff.howl@gmail.com>
2015-02-08 08:04:55 -0800

    added hidden form

--- www/main.js
+++ www/main.js
@@ -1,5 +1,48 @@
 var autocomplete = new google.maps.places.Autocomplete($('.searchbox input')[0], {types:['geocode']} );
 
+$('.searchbox button').click(function(e){
+
+	var place = autocomplete.getPlace();
+
+	address = {
+		street: '',
+		city: '',
+		state: '',
+		zip: ''
+	};
+
+	// translates google places data to form data
+	var addressForm = {
+		street_number: 'street',
+		route: 'street',
+		locality: 'city',
+		administrative_area_level_1: 'state',
+		postal_code: 'zip',
+	}
+
+	// short vs long name config
+	var componentForm = {
+	  street_number: 'short_name',
+	  route: 'long_name',
+	  locality: 'long_name',
+	  administrative_area_level_1: 'short_name',
+	  postal_code: 'short_name',
+	};
+
+	for (var i= 0; i< place.address_components.length; i++) {
+		var addressType = place.address_components[i].types[0];
+		if (componentForm[addressType]) {
+			var val = place.address_components[i][componentForm[addressType]];
+			address[addressForm[addressType]] += (' ' + val);
+		}
+	}
+
+	for (var component in address) 
+		$('#hidden-form [name="address[' + component + ']"]').val(address[component].trim());
+
+	$('#hidden-form').submit();
+});
+
 // dollar sign input field
 function handleChange() {
 var myValue = document.getElementById("income").value;
@@ -12,4 +55,3 @@ var myValue = document.getElementById("income").value;
     document.getElementById("income").value = myValue;
 
 }
-
