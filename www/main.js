85f98262d1c499dd136005f837e99cbfa52d60ca
Thanh Huynh <thanh@Thanhs-MacBook-Pro.local>
2015-02-08 00:04:17 -0800

    add forms and finish half summary

--- www/main.js
+++ www/main.js
@@ -1,1 +1,15 @@
 var autocomplete = new google.maps.places.Autocomplete($('.searchbox input')[0], {types:['geocode']} );
+
+// dollar sign input field
+function handleChange() {
+var myValue = document.getElementById("income").value;
+
+    if (myValue.indexOf("$") != 0)
+    {
+        myValue = "$" + myValue;
+    }
+    
+    document.getElementById("income").value = myValue;
+
+}
+
